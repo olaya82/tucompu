18e5412cae75992e76a10cce075883ca18eeaa4b
olaya82 <olaya82@gmail.com>
2022-06-15 08:49:13 -0300

    modificado menu

--- src/components/MainBar.js
+++ src/components/MainBar.js
@@ -20,8 +20,8 @@ function MainBar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto" >
-                        <Nav.Link href="#home">Portatiles</Nav.Link>
-                        <Nav.Link href="#link">Celulares/Tablets</Nav.Link>
+                        <Nav.Link href="#portatiles">Portatiles</Nav.Link>
+                        <Nav.Link href="#celulares">Celulares/Tablets</Nav.Link>
                             <NavDropdown title="Componentes PC" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#action/3.1">Motherboard</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">Memorias</NavDropdown.Item>
@@ -30,8 +30,8 @@ function MainBar() {
                                 <NavDropdown.Divider />
                                 <NavDropdown.Item href="#action/3.5">Accesorios/Otros</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="#link">Tienda</Nav.Link>
-                            <Nav.Link href="#link">Nosotros</Nav.Link>
+                            <Nav.Link href="#tienda">Tienda</Nav.Link>
+                            <Nav.Link href="#nosotros">Nosotros</Nav.Link>
                         </Nav>
                 </Navbar.Collapse>
             </Container>
